a80079b8a40726c640a73bdae43aff2d4f775e9b
Hafiz Bilal Haider <hafizbilal1999@gmail.com>
2026-06-08 16:39:28 +0500

    Add backend app module with session and employee repository

---
+++ backend/app/repositories/employee.py
@@ -0,0 +1,15 @@
+from sqlmodel import Session, select
+from models import Employee
+
+
+def get_employee_by_id(emp_id: int, session: Session):
+    return session.exec(
+        select(Employee).where(Employee.id == emp_id)
+    ).first()
+
+
+def save_employee(employee: Employee, session: Session):
+    session.add(employee)
+    session.commit()
+    session.refresh(employee)
+    return employee
